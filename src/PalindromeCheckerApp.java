21e335e76c889e4adf18b8a3b71eee0336e73868
pratyushsrivastava1sgn <pratyushsrivastava1sgn@gmail.com>
2026-03-05 23:49:18 +0530

    UC12

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,6 +1,69 @@
+import java.util.Stack;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
 public class PalindromeCheckerApp {
+
     public static void main(String[] args) {
 
-        System.out.println(" Welcome to Palindrome Checker App ");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter string: ");
+        String input = sc.nextLine();
+
+        System.out.print("Choose strategy (1-Stack, 2-Deque): ");
+        int choice = sc.nextInt();
+
+        PalindromeStrategy strategy;
+
+        if (choice == 1) {
+            strategy = new StackStrategy();
+        } else {
+            strategy = new DequeStrategy();
+        }
+
+        boolean result = strategy.check(input);
+        System.out.println(result ? "Palindrome" : "Not Palindrome");
+    }
+}
+
+interface PalindromeStrategy {
+    boolean check(String input);
+}
+
+class StackStrategy implements PalindromeStrategy {
+
+    public boolean check(String input) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : input.toCharArray()) {
+            stack.push(c);
+        }
+
+        for (char c : input.toCharArray()) {
+            if (stack.pop() != c) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+class DequeStrategy implements PalindromeStrategy {
+
+    public boolean check(String input) {
+        Deque<Character> deque = new ArrayDeque<>();
+
+        for (char c : input.toCharArray()) {
+            deque.addLast(c);
+        }
+
+        while (deque.size() > 1) {
+            if (deque.removeFirst() != deque.removeLast()) {
+                return false;
+            }
+        }
+        return true;
     }
 }
