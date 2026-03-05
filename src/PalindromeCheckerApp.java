68501b05577edf0f571fea4fb0a5cd929e9aabed
pratyushsrivastava1sgn <pratyushsrivastava1sgn@gmail.com>
2026-03-05 23:36:37 +0530

    UC5

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,6 +1,31 @@
+import java.util.Stack;
+
 public class PalindromeCheckerApp {
+
     public static void main(String[] args) {
 
-        System.out.println(" Welcome to Palindrome Checker App ");
+        // UC5: Stack-Based Palindrome Checker
+        String input = "noon";
+
+        Stack<Character> stack = new Stack<>();
+
+        // Push characters into stack
+        for (char c : input.toCharArray()) {
+            stack.push(c);
+        }
+
+        boolean isPalindrome = true;
+
+        // Pop and compare
+        for (char c : input.toCharArray()) {
+            if (c != stack.pop()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Print result
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
     }
 }
