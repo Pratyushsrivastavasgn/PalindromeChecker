8871fb15f2c2a81cfa4ef3acbad5121b435cb8e1
pratyushsrivastava1sgn <pratyushsrivastava1sgn@gmail.com>
2026-03-05 23:35:13 +0530

    UC4

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,6 +1,32 @@
 public class PalindromeCheckerApp {
+
     public static void main(String[] args) {
 
-        System.out.println(" Welcome to Palindrome Checker App ");
+        // UC4: Character Array Based Palindrome Check
+        String text = "radar";
+
+        // Convert string to character array
+        char[] chars = text.toCharArray();
+
+        int start = 0;
+        int end = chars.length - 1;
+        boolean isPalindrome = true;
+
+        // Two-pointer comparison
+        while (start < end) {
+            if (chars[start] != chars[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        // Print result
+        if (isPalindrome) {
+            System.out.println(text + " is a palindrome.");
+        } else {
+            System.out.println(text + " is NOT a palindrome.");
+        }
     }
 }
