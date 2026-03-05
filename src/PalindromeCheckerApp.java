f22d1b500d4088e6566067426ff5ff17e8586ae3
pratyushsrivastava1sgn <pratyushsrivastava1sgn@gmail.com>
2026-03-05 23:33:15 +0530

    UC3

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,6 +1,25 @@
 public class PalindromeCheckerApp {
+
     public static void main(String[] args) {
 
-        System.out.println(" Welcome to Palindrome Checker App ");
+        // UC3: Palindrome Check Using String Reverse
+        String input = "level";
+        String reverse = "";
+
+        // Reverse the string using for loop
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reverse = reverse + input.charAt(i);
+        }
+
+        // Compare original and reversed string
+        if (input.equals(reverse)) {
+            System.out.println("Input String: " + input);
+            System.out.println("Reversed String: " + reverse);
+            System.out.println("Result: It is a Palindrome");
+        } else {
+            System.out.println("Input String: " + input);
+            System.out.println("Reversed String: " + reverse);
+            System.out.println("Result: It is NOT a Palindrome");
+        }
     }
 }
