5c207e4577e1e81df1497732d6d00777b4efee80
pratyushsrivastava1sgn <pratyushsrivastava1sgn@gmail.com>
2026-03-05 23:40:45 +0530

    UC6

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,6 +1,36 @@
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.Stack;
+
 public class PalindromeCheckerApp {
+
     public static void main(String[] args) {
 
-        System.out.println(" Welcome to Palindrome Checker App ");
+        String input = "civic";
+
+        Queue<Character> queue = new LinkedList<>();
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+            queue.add(ch);
+            stack.push(ch);
+        }
+
+        boolean isPalindrome = true;
+
+        while (!queue.isEmpty()) {
+            if (queue.remove() != stack.pop()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println("UC6 Result: " + input + " is a palindrome.");
+        } else {
+            System.out.println("UC6 Result: " + input + " is NOT a palindrome.");
+        }
     }
 }
